9e6d1e3929f894eae50aab03e1775aa008e7a836
emboy <95904552+emboyip@users.noreply.github.com>
2024-05-17 21:37:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ plugin = dict(root="plugin")
 
 app = Client(
     name="Privet_Downloader",
-    api_id=26471977,
-    api_hash="d79686c0a3ad440560c342c609dede66",
-    phone_number="212703481994",
+    api_id=1234,
+    api_hash="",
+    phone_number="",
     plugins=plugin,
     device_model="Iphone 15 ProMax"
 )
